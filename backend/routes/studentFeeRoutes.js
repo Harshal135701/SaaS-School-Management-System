b0a0ce2f6248e35cfb1f71f5ba445f22b8377311
Harshal135701 <borseharshal1357@gmail.com>
2026-09-09 12:25:20 +0530

    Feat:now we can see the fee paid , fee pending and all

--- backend/routes/studentFeeRoutes.js
+++ backend/routes/studentFeeRoutes.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const { createStudentFee,getStudentFees } = require("../controllers/studentFeeController");
+const {
+    createStudentFee,
+    getStudentFees,
+    getFeeSummary,
+} = require("../controllers/studentFeeController"); 
 const franchiseProtect = require("../middleware/franchiseAuthMiddleware");
 
 router.use(franchiseProtect);
 
 router.post("/", createStudentFee);
 router.get("/", getStudentFees);
+router.get("/summary", getFeeSummary);
 
 module.exports = router;
