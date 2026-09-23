2d5010170e7dfec6ab048ef95a556ef7638049be
Harshal135701 <borseharshal1357@gmail.com>
2026-09-23 22:58:35 +0530

    Feat:fix ai

--- Frontend/src/services/api.ts
+++ Frontend/src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://saas-school-management-system.onrender.com/api"
-  // baseURL:"http://localhost:5000/api"
+  baseURL: "https://saas-school-management-system.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
